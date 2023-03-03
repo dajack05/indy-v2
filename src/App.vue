5505a79ebc2b232b4af9b28c571188e76948e695
Daniel Jackson <dajack05@gmail.com>
2023-03-03 10:40:48 -0500

    inital

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="container">
+    <RouterView />
+  </div>
+</template>
+
+<style>
+html,
+body {
+  background-color: var(--bs-dark);
+  color: var(--bs-light);
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Welcome at {{time}}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        const time = new Date().toTimeString();
+        return{
+            time,
+        }
+    },
+})
+</script>
