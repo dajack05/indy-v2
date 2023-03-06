fbdaf245687d0e6dfd4e840bf3c7efdeaf13f8f5
Daniel Jackson <dajack05@gmail.com>
2023-03-06 16:48:10 -0500

    update

--- server/tests/handlers/UserHandlers.test.ts
+++ server/tests/handlers/UserHandlers.test.ts
@@ -1,47 +1,57 @@
-import { Request, Response, request } from "express"
+import { Request, Response, request } from "express";
 import { UserHandlers } from "../../src/handlers/UserHandlers";
 import { Context, MockContext, createMockContext } from "../../src/Context";
-import { UserStore } from "../../src/stores/UserStore";
+import { User, UserStore } from "../../src/stores/UserStore";
 
 let mockCtx: MockContext;
 let ctx: Context;
 
 describe("UserHandlers Tests", () => {
+  beforeEach(() => {
+    mockCtx = createMockContext();
+    ctx = mockCtx as unknown as Context;
+  });
 
-    beforeEach(() => {
-        mockCtx = createMockContext();
-        ctx = mockCtx as unknown as Context;
+  afterEach(() => jest.clearAllMocks());
+
+  test("Valid ID returns valid user", () => {
+    const req = {
+      params: { id: "1" },
+    };
+
+    const res = {
+      text: "",
+      send: function (input: string) {
+        this.text = input;
+      },
+    };
+
+    jest.spyOn(UserStore, "GetByID");
+    jest.mock("../../src/stores/UserStore",()=>{
+        default:()=>{},
     });
 
-    test("Valid ID returns valid user", () => {
-        const req = {
-            params: { id: "1" }
-        };
+    UserHandlers.GetUserByID(
+      ctx,
+      req as unknown as Request,
+      res as unknown as Response
+    );
 
-        const res = {
-            text: '',
-            send: function (input: string) { this.text = input }
-        };
+    expect(UserStore.GetByID).toBeCalled();
+  });
 
-        jest.mock("../../src/stores/UserStore");
+  // test("Missing ID returns empty", () => {
+  //     const req = {
+  //         params: {}
+  //     };
 
-        UserHandlers.GetUserByID(ctx, req as unknown as Request, res as unknown as Response);
+  //     const res = {
+  //         text: '',
+  //         send: function (input: string) { this.text = input }
+  //     };
 
-        expect(UserStore.GetByID).toBeCalled();
-    });
+  //     UserHandlers.GetUserByID(ctx, req as unknown as Request, res as unknown as Response);
 
-    test("Missing ID returns empty", () => {
-        const req = {
-            params: {}
-        };
-
-        const res = {
-            text: '',
-            send: function (input: string) { this.text = input }
-        };
-
-        UserHandlers.GetUserByID(ctx, req as unknown as Request, res as unknown as Response);
-
-        expect(res.text).toBe("");
-    })
-})
+  //     expect(res.text).toBe("");
+  // })
+});
